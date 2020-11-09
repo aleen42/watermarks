c40b383417482bf3e7102444eea3abb9c20c7761
Aleen <aleen42@vip.qq.com>
2020-11-09 14:13:15 +0800

    perf(watermarks): simplify implementation
    1. remove the dependency of jQuery
    2. remove Chinese comments
    3. fix the definition of scrollableElement
    4. simplify Watermarks.prototype.reset

--- index.js
+++ index.js
@@ -14,15 +14,15 @@
  *  - Author: aleen42
  *  - Description: main entry of the watermark component
  *  - Create Time: Apr 24th, 2019
- *  - Update Time: Apr 24th, 2019
+ *  - Update Time: Nov 9th, 2020
  *
  */
-const $ = require('jquery');
 
 /**
- * @param $target which target you want to append watermarks upon
- * @param params defined as following thumbnail
- * @param $scrollableElements
+ * @param target - which target you want to append watermarks upon
+ * @param params - defined as following thumbnail
+ * @param {object} scrollableElement - x: axis-x scrollable element
+ *                                   - y: axis-y scrollable element
  *  -----------------------------------------
  * |   |ox: 300(px)|    |                   |   DEBUG_MODE  : debug canvas drawing calculation
  * |   |___________|      align: left       |   contents    : an array of text contents for drawing watermarks
@@ -41,48 +41,48 @@ const $ = require('jquery');
  * -----------------------------------------
  * @constructor
  */
-const Watermarks = window.Watermarks = window.Watermarks || function ($target, params, $scrollableElements) {
-    /** todo: can I calculate scrollable parents from the $target element */
-    const SCROLLABLE_MAX_WIDTH = $target[0].scrollWidth;
-    const SCROLLABLE_MAX_HEIGHT = $target[0].scrollHeight;
+const Watermarks = window.Watermarks = window.Watermarks || function (target, params, {x: scrollX, y: scrollY} = {}) {
+    // todo: can I calculate scrollable parents from the target element
+    const SCROLLABLE_MAX_WIDTH = target.scrollWidth;
+    const SCROLLABLE_MAX_HEIGHT = target.scrollHeight;
 
     let CANVAS_DEFAULT_WIDTH = SCROLLABLE_MAX_WIDTH;
     let CANVAS_DEFAULT_HEIGHT = SCROLLABLE_MAX_HEIGHT;
-    if ($scrollableElements) {
-        const $x = $scrollableElements.x || $scrollableElements;
-        const $y = $scrollableElements.y || $scrollableElements;
-
-        CANVAS_DEFAULT_WIDTH = $x.width() > $(window).width() ? $(window).width() : $x.width();
-        CANVAS_DEFAULT_HEIGHT = $y.height() > $(window).height() ? $(window).height() : $y.height();
+    if (scrollX || scrollY) {
+        const wW = window.innerWidth, wH = window.innerHeight, sW = scrollX.scrollWidth, sH = scrollY.scrollHeight;
+        CANVAS_DEFAULT_WIDTH = sW > wW ? wW : sW;
+        CANVAS_DEFAULT_HEIGHT = sH > wH ? wH : sH;
     }
 
-    let $canvas = $target.find('.j-watermark');
+    let canvas = target.querySelector('.j-watermark');
 
-    /** create a new Canvas element */
-    if ($canvas.length === 0) {
-        $canvas = $('<canvas class="j-watermark" style="position: absolute; top: 0; left: 0; z-index: 1; pointer-events: none"></canvas>');
-        $target.append($canvas);
+    // create a new Canvas element
+    if (!canvas) {
+        canvas = document.createElement('canvas');
+        canvas.className = 'j-watermark';
+        canvas.setAttribute('style', 'position: absolute; top: 0; left: 0; z-index: 1; pointer-events: none');
+        target.appendChild(canvas);
     }
 
-    this.canvas = $canvas[0];
-    $target.css('position', 'relative');
+    this.canvas = canvas;
+    target.style.position = 'relative';
 
-    /** initiate default params */
-    params = $.extend(true, {
+    // initiate default params
+    params = Object.assign({
         DEBUG_MODE: false,
         watermarks: {
             region: {
-                x: 300, /** ox, 300 by default */
-                y: 300, /** oy, 300 by default */
-                width: 150, /** ow, 150 by default */
-                height: 150, /** oh, 150 by default */
+                x: 300, // ox, 300 by default
+                y: 300, // oy, 300 by default
+                width: 150, // ow, 150 by default
+                height: 150, // oh, 150 by default
             },
-            align: 'left', /** text-align, left by default */
-            alpha: 0.2, /** font-alpha, 0.2 by default */
-            fontSize: 16, /** font-size, 16 by default */
-            fontRotate: Math.PI / 4, /** font-rotate, -45° by default */
-            lineHeightRatio: 1.5, /** line-height, 1.5x by default */
-            reservedRatio: 9, /** the times for extending reserved region, which can be increased  */
+            align: 'left', // text-align, left by default
+            alpha: 0.2, // font-alpha, 0.2 by default
+            fontSize: 16, // font-size, 16 by default
+            fontRotate: Math.PI / 4, // font-rotate, -45° by default
+            lineHeightRatio: 1.5, // line-height, 1.5x by default
+            reservedRatio: 9, // the times for extending reserved region, which can be increased
         },
     }, params);
 
@@ -91,7 +91,7 @@ const Watermarks = window.Watermarks = window.Watermarks || function ($target, p
 
     const _init = () => {
         /**
-         * extend: Do not use following optimization when not passing $scrollElement
+         * extend: Do not use following optimization when not passing scrollableElement
          *
          * optimization: in order to render in a smaller canvas, rather than creating a huge, there
          *               are two proposals.
@@ -105,11 +105,11 @@ const Watermarks = window.Watermarks = window.Watermarks || function ($target, p
             SCROLLABLE_MAX_HEIGHT > CANVAS_DEFAULT_HEIGHT ? reservedRatio * region.y : 0,
         ];
 
-        /** set the size of the canvas */
-        this.canvas.width = CANVAS_DEFAULT_WIDTH + rw;
-        this.canvas.height = CANVAS_DEFAULT_HEIGHT + rh;
+        // set the size of the canvas
+        canvas.width = CANVAS_DEFAULT_WIDTH + rw;
+        canvas.height = CANVAS_DEFAULT_HEIGHT + rh;
 
-        /** register scroll events */
+        // register scroll events
         const previous = {};
         const axisConditions = {
             x: SCROLLABLE_MAX_WIDTH > CANVAS_DEFAULT_WIDTH,
@@ -117,72 +117,72 @@ const Watermarks = window.Watermarks = window.Watermarks || function ($target, p
         };
 
         if (axisConditions.x || axisConditions.y) {
-            /** set position of the canvas */
-            const checkCanvasPosition = specificAxis => {
-                $.each({
-                    x: ['left', rw, SCROLLABLE_MAX_WIDTH - this.canvas.width, 'scrollLeft'],
-                    y: ['top', rh, SCROLLABLE_MAX_HEIGHT - this.canvas.height, 'scrollTop'],
-                }, (axis, [property, reserved, limit, fn]) => {
+            // set position of the canvas
+            const checkPos = specificAxis => {
+                Object.entries({
+                    x: ['left', rw, SCROLLABLE_MAX_WIDTH - canvas.width, 'scrollLeft'],
+                    y: ['top', rh, SCROLLABLE_MAX_HEIGHT - canvas.height, 'scrollTop'],
+                }).forEach(([axis, [property, reserved, limit, key]]) => {
                     if (!axisConditions[axis] || (specificAxis && axis !== specificAxis)) return;
 
-                    /** 保证偶数个 ox 或 oy 距离 */
-                    /** ensure that moving even distances of ox or oy */
-                    let numbers = Math.floor(Math.floor($scrollableElements[axis][fn]() / reserved) * reserved / region[axis]);
+                    // ensure that moving even distances of ox or oy
+                    let numbers = Math.floor(Math.floor((axis === 'x' ? scrollX : scrollY)[key] / reserved) * reserved / region[axis]);
                     numbers -= numbers % 2;
 
-                    /** 保证在 scrollable area 内渲染 */
-                    /** guarantee that rendering inside the scrollable area */
+                    // guarantee that rendering inside the scrollable area
                     let val = numbers * region[axis];
                     val = val >= limit ? limit : val;
 
                     if (previous[axis] !== val) {
-                        /** reset positions when scrolling to specific places */
+                        // reset positions when scrolling to specific places
                         previous[axis] = val;
-                        $canvas.css(property, `${val}px`);
+                        canvas.style[property] = `${val}px`;
                     }
                 });
             };
 
-            checkCanvasPosition();
+            checkPos();
 
-            if ($scrollableElements.x === $scrollableElements.y) {
-                $scrollableElements.x.scroll(checkCanvasPosition.bind(null, ''));
+            if (scrollX || scrollY) {
+                if (scrollX === scrollY) {
+                    scrollX && scrollX.addEventListener('scroll', checkPos.bind(0, ''));
+                } else {
+                    scrollX && scrollX.addEventListener('scroll', checkPos.bind(0, 'x'));
+                    scrollY && scrollY.addEventListener('scroll', checkPos.bind(0, 'y'));
+                }
             } else {
-                $scrollableElements.x.scroll(checkCanvasPosition.bind(null, 'x'));
-                $scrollableElements.y.scroll(checkCanvasPosition.bind(null, 'y'));
+                target.addEventListener('scroll', checkPos.bind(0, ''));
             }
         }
 
-        /** filter empty string */
+        // filter empty string
         params.contents = params.contents.filter(content => content.length);
     };
 
     const _draw = () => {
-        const context = this.canvas.getContext('2d');
+        const context = canvas.getContext('2d');
 
-        /** clear at first */
-        context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        // clear at first
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-        for (let x = 0; x < this.canvas.width; x += region.x) {
-            for (let y = 0; y < this.canvas.height; y += region.y) {
+        for (let x = 0; x < canvas.width; x += region.x) {
+            for (let y = 0; y < canvas.height; y += region.y) {
                 context.save();
 
                 context.globalAlpha = watermarks.alpha;
                 context.font = `${watermarks.fontSize}px '微软雅黑'`;
 
-                /** 为了避免奇偶行水印重叠，基数行偏移 */
-                /** translate pivot when it is an odd row to avoid reduplication */
+                // translate pivot when it is an odd row to avoid reduplication
                 context.translate(x + (y / region.y + 1) % 2 * region.width, y);
 
-                params.DEBUG_MODE /** DEBUG */ && context.fillRect(0, 0, params.watermarks.region.width, params.watermarks.region.height);
+                params.DEBUG_MODE && context.fillRect(0, 0, params.watermarks.region.width, params.watermarks.region.height);
 
-                /** todo: need to consider degrees out of the range between 0 and 90 degrees */
+                // todo: need to consider degrees out of the range between 0 and 90 degrees
                 const sita = watermarks.fontRotate;
                 context.rotate(-sita);
                 context.textAlign = watermarks.align;
 
                 /**
-                 * 计算字体填充相对矩形的中间位置
                  * calculate the center position of each content responding to the rectangle
                  * https://codepen.io/aleen42/pen/KRPPVW
                  *
@@ -199,8 +199,7 @@ const Watermarks = window.Watermarks = window.Watermarks || function ($target, p
                  *      | x  x    |
                  *      x -x-------                dy = (oh/2+ow/2)*sin-fh/2+fs*cos
                  *
-                 * Note: y 方向需要增加 fs*cos 偏移量才能使得内容的左上角对齐水印区域的左上角
-                 *       https://codepen.io/aleen42/pen/RMXXqv
+                 * Note: axis-y need a value of fs * cos to align (https://codepen.io/aleen42/pen/RMXXqv)
                  */
                 const fontSize = watermarks.fontSize;
                 const lineHeight = watermarks.lineHeightRatio * fontSize;
@@ -212,37 +211,38 @@ const Watermarks = window.Watermarks = window.Watermarks || function ($target, p
                 ];
 
                 const [dx, dy] = [
-                    region.width / 2 * (1 - sin * cos) / cos - fw / 2 - region.height / 2 * sin, /** display axis-x value */
-                    (region.height / 2 + region.width / 2) * sin - fh / 2 + fs * cos, /** display axis-y value */
+                    region.width / 2 * (1 - sin * cos) / cos - fw / 2 - region.height / 2 * sin, // display axis-x value
+                    (region.height / 2 + region.width / 2) * sin - fh / 2 + fs * cos, // display axis-y value
                 ];
 
                 [
-                    [['fillStyle', 'black'], 'fillText'], /** background rendering */
-                    [['strokeStyle', 'white'], 'strokeText'], /** border rendering, in order to avoid color reduplication */
+                    [['fillStyle', 'black'], 'fillText'], // background rendering
+                    [['strokeStyle', 'white'], 'strokeText'], // border rendering, in order to avoid color reduplication
                 ].forEach(([[style, color], draw]) => {
                     context[style] = color;
                     params.contents.forEach((content, index) => context[draw](content, dx, dy + lineHeight * index));
                 });
 
-                params.DEBUG_MODE /** DEBUG */ && (context.strokeStyle = '#a10000');
-                params.DEBUG_MODE /** DEBUG */ && context.strokeRect(dx, dy - fontSize * cos, fw, lineHeight + fontSize);
+                params.DEBUG_MODE && (context.strokeStyle = '#a10000');
+                params.DEBUG_MODE && context.strokeRect(dx, dy - fontSize * cos, fw, lineHeight + fontSize);
 
                 context.restore();
             }
         }
     };
 
-    /** 部分浏览器需要等待 canvas 渲染后才能进行绘图 */
-    /** some browsers will need to fill or stroke after canvas has been rendered */
+    // some browsers will need to fill or stroke after canvas has been rendered
     setTimeout(() => {
         _init();
         _draw();
     }, 0);
 };
 
-Watermarks.prototype.reset = function () {
-    this.canvas.width = 0;
-    this.canvas.height = 0;
-};
+// noinspection JSUnusedGlobalSymbols
+Object.assign(Watermarks.prototype, {
+    reset() {
+        this.canvas.width = this.canvas.height = 0;
+    },
+});
 
 module.exports = Watermarks;
